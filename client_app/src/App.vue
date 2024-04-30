6147f2ccdb156b0d616ce47c63d2065dc4a11714
kks1030 <201900543@hufs.ac.kr>
2024-04-30 22:14:04 +0900

    app.vue_변경

--- client_app/src/App.vue
+++ client_app/src/App.vue
@@ -3,10 +3,10 @@
 </template>
 
 <style lang="scss">
-@import 'scss/main.scss';
+@import "scss/main.scss";
 
 #app {
-  font-family: 'Inter', Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Inter", Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
