656deaf88ce36d37ea24e003ad5c1dbf24e4e0cc
kks1030 <201900543@hufs.ac.kr>
2024-05-02 13:38:18 +0900

    불필요 파일 삭제

--- client_app/src/main.ts
+++ client_app/src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+import "./style.css";
 import i18n from "./i18n";
 import { createVuestic } from "vuestic-ui";
 
--- client_app/src/pages/admin/dashboard/cards/Timeline.vue
+++ client_app/src/pages/admin/dashboard/cards/Timeline.vue
@@ -10,23 +10,21 @@ import VaTimelineItem from "../../../../components/va-timeline-item.vue";
     <VaCardContent>
       <table class="mt-4">
         <tbody>
-          <VaTimelineItem date="2023년 3월">
-            회사 창립자들이 창업 시작
-          </VaTimelineItem>
+          <VaTimelineItem date="2023년 3월"> 회사 창업 </VaTimelineItem>
           <VaTimelineItem date="2023년 6월">
-            첫 대형 프로젝트 계약을 성공적으로 체결
+            첫 대형 프로젝트 계약 성공적으로 체결
           </VaTimelineItem>
           <VaTimelineItem date="2023년 9월">
-            회사는 번역 서비스를 제공하기 시작
+            번역 서비스 제공 시작
           </VaTimelineItem>
           <VaTimelineItem date="2023년 12월">
             첫 해외 프로젝트를 런칭
           </VaTimelineItem>
           <VaTimelineItem date="2024년 2월">
-            회사 매출 창립 이래 최고치를 기록
+            회사 매출 창립 이래 최고치 기록
           </VaTimelineItem>
           <VaTimelineItem date="2024년 5월">
-            새로운 사업부문을 추가하여 서비스 범위를 확장
+            새로운 사업부문 추가하여 서비스 범위 확장
           </VaTimelineItem>
         </tbody>
       </table>
